92c43c32779d96726eb7bae1f5a592927286abcf
Muhammad <muhammadmamushah.com@gmail.com>
2022-09-27 19:46:49 +0500

    Service: GetAll Search

---
+++ src/ZaminEducation.Service/Interfaces/IHashTagService.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq.Expressions;
+using ZaminEducation.Domain.Entities.Courses;
+
+namespace ZaminEducation.Service.Interfaces
+{
+    public interface IHashTagService
+    {
+        ValueTask<IEnumerable<HashTag>> GetAll(Expression<Func<HashTag, bool>> expression);
+    }
+}
---
+++ src/ZaminEducation.Service/Services/HashTagService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using ZaminEducation.Data.IRepositories;
+using ZaminEducation.Domain.Entities.Courses;
+using ZaminEducation.Service.Interfaces;
+
+namespace ZaminEducation.Service.Services
+{
+    public class HashTagService : IHashTagService
+    {
+        private readonly IRepository<HashTag> hashTagRepository;
+
+        public HashTagService(IRepository<HashTag> hashTagRepository)
+        {
+            this.hashTagRepository = hashTagRepository;
+        }
+
+        public async ValueTask<IEnumerable<HashTag>> GetAll(Expression<Func<HashTag, bool>> expression) =>
+            await hashTagRepository.GetAll(expression, isTracking: false).ToListAsync();
+    }
+}
